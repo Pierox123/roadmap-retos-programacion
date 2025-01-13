0991e9124c3103a3c3c684142e6219784635dc32
Pierox123 <rpiero1234@gmail.com>
2025-01-13 15:13:35 -0500

    Create Pierox123.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Pierox123.py
@@ -0,0 +1,32 @@
+# ejercicio 1 
+# https://www.python.org/
+# el numeral se usa si quieres abrir un texto o mencionar algo
+# ejercicio 2
+# esto es un comentario en una linea 
+'''
+esto es un comentario
+en unas o mas lineas
+'''
+"""
+tambien se puede hacer
+con comillas doble
+"""
+# ejercicio 3
+my_variable = "mi variable"
+
+MY_CONSTANT = "mi constante
+# la constante en python no existe, pero se puede expresar con una variable con full mayusculas
+
+#ejercicio 4
+# datos primitivos
+numero_int = 2
+# este es para numeros enteros
+numero_float = 3.5
+# esto es para numero decimales
+my_bool = True
+my_bool = False
+# esto se usa para poner el valor verdadero o falso
+my_string = "cadena de texto"
+
+#ejercicio 5
+print("!Hola python!")
