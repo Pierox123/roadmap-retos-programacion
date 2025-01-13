5bae5d9d6052238f1110bbb40654a75921fc2d82
Pierox123 <rpiero1234@gmail.com>
2025-01-13 14:58:37 -0500

    Create Pierox1234.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Pierox1234.py
@@ -0,0 +1,28 @@
+# Ejercicio 1
+# https://www.python.org/
+# Ejercicio 2
+# Comentario en una linea
+'''
+Este 
+comentario
+es en varias lineas,
+se cierra con comillas
+'''
+"""
+Tambien se puede
+usar comillas dobles
+"""
+# Ejercicio 3
+# Tipos de datos primitivos
+CONSTANTE = "esta solo es una manera de poner una constante, que seria escribirlo en mayusculas, pero estas dentro de phyton no existen"
+variable_float = 10.5
+# aqui se escriben numero decimales
+variable_int = 7
+# aqui se escriben numeros enteros
+variable_bool = False
+variable_bool = True
+# esta se usa para poner los valores de falso y verdadero
+variable_string = "Hola Phyton"
+
+# ejercicio 4
+print("Hola phyton!")
